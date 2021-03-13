27cfe69a7105c2c4489c9808430affa78267737b
Abdul Omar Faruq <abdulomarfaruq@gmail.com>
2021-03-13 04:27:54 +0000

    Added route lines Added custom route coordinates in json

--- lib/mapbox/mapbox_page.dart
+++ lib/mapbox/mapbox_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 import 'constants_mapbox.dart';
@@ -9,13 +12,48 @@ class MapboxMapPage extends StatefulWidget {
 }
 
 class _MapboxMapState extends State<MapboxMapPage> {
+  List _coordinatesData = [];
   MapboxMapController mapController;
 
   void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
+    _addRoute();
   }
 
   void onStyleLoadedCallback() {}
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString('lib/mapbox/mapbox_asset/coordinates.json');
+    final data = await json.decode(response);
+
+    setState(() {
+      _coordinatesData = List.from(data['geometry']['coordinates']);
+    });
+  }
+
+  Future<List<LatLng>> geometryList() async {
+    await readJson();
+    List<LatLng> geometryVal = [];
+
+    for (var i = 0; i < _coordinatesData.length; i++) {
+      geometryVal.add(LatLng(_coordinatesData[i][0], _coordinatesData[i][1]));
+    }
+    return geometryVal;
+  }
+
+  void _addRoute() async {
+    List geometryListData = await geometryList();
+    mapController.addLine(
+      LineOptions(
+          geometry: geometryListData,
+          lineColor: "#ff0000",
+          lineWidth: 14.0,
+          lineOpacity: 1,
+          lineJoin: "round",
+          draggable: false),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
