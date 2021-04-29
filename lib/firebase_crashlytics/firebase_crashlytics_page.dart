ed66d97de82b8a8943afe59af846255e4e2fb224
Abdul Omar Faruq <abdulomarfaruq@gmail.com>
2021-04-29 15:10:13 +0100

    made crashlytics page responsive

--- lib/firebase_crashlytics/firebase_crashlytics_page.dart
+++ lib/firebase_crashlytics/firebase_crashlytics_page.dart
@@ -15,6 +15,7 @@ class FirebaseCrashlyticsPage extends StatelessWidget {
     _pageNavigator.setCurrentPageIndex =
         _pageNavigator.getPageIndex('Firebase Crashlytics');
     _pageNavigator.setFromIndex = _pageNavigator.getCurrentPageIndex;
+    MediaQueryData deviceData = MediaQuery.of(context);
 
     FirebaseCrashlytics fci = FirebaseCrashlytics.instance;
     String message =
@@ -38,32 +39,94 @@ class FirebaseCrashlyticsPage extends StatelessWidget {
       onPressCrash = null;
     }
 
-    return Card(
-      child: Container(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.red.shade100,
-              height: 200,
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  message,
-                  style: TextStyle(
-                    fontSize: 20.0,
+    Widget landscapeViewWidget = Card(
+      child: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red.shade100,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: deviceData.size.height * 0.5,
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        message,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: onPressCrash,
+                    icon: Icon(Icons.error_rounded),
+                    label: Text('Press to crash'),
+                  ),
+                ),
+              ],
             ),
-            ElevatedButton.icon(
-              onPressed: onPressCrash,
-              icon: Icon(Icons.error_rounded),
-              label: Text('Press to crash'),
-            ),
-          ],
+          ),
         ),
       ),
     );
+
+    Widget portraitViewWidget = Card(
+      child: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red.shade100,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: deviceData.size.height * 0.3,
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        message,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: deviceData.size.width * 0.6,
+                    child: ElevatedButton.icon(
+                      onPressed: onPressCrash,
+                      icon: Icon(Icons.error_rounded),
+                      label: Text('Press to crash'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    return deviceData.orientation == Orientation.portrait
+        ? portraitViewWidget
+        : landscapeViewWidget;
   }
 }
